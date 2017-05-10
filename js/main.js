a807d7a657f58181cc8b433c040f99247e074acf
christinacthomas <christinacthomas@gmail.com>
2017-05-09 19:43:03 -0700

    Classword

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// document.getElementsByClassName("are")[0].backgroundColor = "pink"
+
+var newDi= document.createElement ('div')
+newDi.className ="we"
+
+var textHere = document.createTextNode ('im so tired')
+
+newDi.appendChild(textHere)
+
+// var adhere = document.getElementsByClassName ('')[0];
+// adhere.appendChild(newDi)
+
+// document.getElementById ('error').innerHTML = "<h1>testing</h1>"
+
+var newHeader = document.createElement ('input')
+var text = document.createTextNode ('testing')
+newHeader.appendChild(text)
+//<h1>testing</h1>
+
+var are = document.getElementById ('are')
+are.appendChild(newHeader)
